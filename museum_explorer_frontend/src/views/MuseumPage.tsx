43fbc354a9c31b36e64af4b59302a209463ba548
GOMES Mathéo <matheo.gomes@externe.maif.fr>
2026-07-31 13:52:40 +0200

    Working views/MuseumPage component

---
+++ museum_explorer_frontend/src/views/MuseumPage.tsx
@@ -0,0 +1,10 @@
+import MuseumList from "../components/organisms/MuseumList";
+
+export default function MuseumPage() {
+    return(
+        <>
+            <h1>List of aivailable museums</h1>
+            <MuseumList/>
+        </>
+    )
+}
