44f34ac1d265a84e9f0de8ea78d79b53d7952b90
GOMES Mathéo <matheo.gomes@externe.maif.fr>
2026-07-31 14:04:11 +0200

    React rooter + default route ('/', list of museums)

--- museum_explorer_frontend/src/App.tsx
+++ museum_explorer_frontend/src/App.tsx
@@ -1,11 +1,20 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "./App.css";
+import MuseumPage from "./views/MuseumPage";
 
 function App() {
+  let router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MuseumPage />,
+    },
+    {
+      path: "*",
+      element: <span>NOT IMPLEMENTED</span>,
+    },
+  ]);
 
-  return (
-    <>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
