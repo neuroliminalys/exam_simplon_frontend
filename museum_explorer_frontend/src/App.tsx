685a8c9749df04d18b05e5c2a6cc6fd63f33ef3e
Neuroliminalys-02 <147269298+neuroliminalys@users.noreply.github.com>
2026-07-31 14:47:41 +0200

    Lazy and Suspense working integration (#33)
    Co-authored-by: GOMES Mathéo <matheo.gomes@externe.maif.fr>

--- museum_explorer_frontend/src/App.tsx
+++ museum_explorer_frontend/src/App.tsx
@@ -1,17 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import "./App.css";
-import MuseumPage from "./views/MuseumPage";
-import MuseumDetailsPage from "./views/MuseumDetailsPage";
+import { lazy, Suspense, type LazyExoticComponent, type ReactNode } from "react";
+
+const MuseumPage = lazy(() => import("./views/MuseumPage"))
+const MuseumDetailsPage = lazy(() => import("./views/MuseumDetailsPage"))
+
+function Suspend(Component: LazyExoticComponent<(() => ReactNode)>) {
+  return (<Suspense fallback={<p>Loading...</p>}>
+    {<Component/>}
+  </Suspense>)
+}
 
 function App() {
   let router = createBrowserRouter([
     {
       path: "/",
-      element: <MuseumPage />,
+      element: Suspend(MuseumPage),
     },
     {
       path: "/details/:id",
-      element: <MuseumDetailsPage />,
+      element: Suspend(MuseumDetailsPage),
     },
     {
       path: "*",
