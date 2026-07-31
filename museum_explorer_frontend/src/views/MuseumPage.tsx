040ff79c248126cacf69080dd388a667463a38f9
Neuroliminalys-02 <147269298+neuroliminalys@users.noreply.github.com>
2026-07-31 17:00:23 +0200

    Images + fixing mispelling (#40)
    Co-authored-by: GOMES Mathéo <matheo.gomes@externe.maif.fr>

--- museum_explorer_frontend/src/views/MuseumPage.tsx
+++ museum_explorer_frontend/src/views/MuseumPage.tsx
@@ -3,7 +3,7 @@ import MuseumList from "../components/organisms/MuseumList";
 export default function MuseumPage() {
     return(
         <>
-            <h1>List of aivailable museums</h1>
+            <h1>List of available museums</h1>
             <MuseumList/>
         </>
     )
